cc4f18b0b880cd5402c271fe900a9973badaa046
Teemu Karjalainen <teemu.karjalainen0@gmail.com>
2025-06-24 16:02:33 +0300

    Change to createWebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Career from '../pages/Career.vue';
 import Projects from '../pages/Projects.vue';
 
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory('/portfolio/'),
+    history: createWebHashHistory(),
     routes
 });
 
